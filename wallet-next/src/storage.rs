5b50b3c9a8f671c1baa75296e641bfb322f5c11d
aubrey <aubrey@penumbra.zone>
2022-05-04 17:47:10 -0700

    Add commit logic to wallet-next storage

--- wallet-next/src/storage.rs
+++ wallet-next/src/storage.rs
@@ -1,7 +1,13 @@
 use penumbra_chain::params::ChainParams;
-use penumbra_crypto::merkle::NoteCommitmentTree;
+use penumbra_crypto::{
+    merkle::{NoteCommitmentTree, Tree},
+    note::Commitment,
+    FieldExt,
+};
 use penumbra_proto::{crypto::FullViewingKey, Message, Protobuf};
-use sqlx::{query, Pool, Sqlite};
+use sqlx::{query, Executor, Pool, Sqlite};
+
+use crate::sync::ScanResult;
 
 #[derive(Clone)]
 pub struct Storage {
@@ -79,5 +85,74 @@ impl Storage {
         Ok(bincode::deserialize(
             result[0].bytes.as_ref().unwrap().as_slice(),
         )?)
+    }
+
+    pub async fn record_block(
+        &self,
+        scan_result: ScanResult,
+        nct: &mut NoteCommitmentTree,
+    ) -> anyhow::Result<Self> {
+        // Insert all new note records
+        for note_record in scan_result.new_notes {
+            let query = format!(
+                r#"INSERT INTO notes (
+                        note_commitment,
+                        height_spent,
+                        height_created,
+                        diversifier,
+                        amount,
+                        asset_id,
+                        transmission_key,
+                        blinding_factor,
+                        diversifier_index,
+                        nullifier)
+                    VALUES ({:?},NULL,{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?})"#,
+                note_record.note_commitment.0.to_bytes(),
+                //height_spent is NULL
+                scan_result.height,
+                note_record.note.diversifier().0,
+                note_record.note.amount(),
+                note_record.note.asset_id().to_bytes(),
+                note_record.note.transmission_key().0,
+                note_record.note.note_blinding().to_bytes(),
+                note_record.diversifier_index.0,
+                note_record.nullifier.to_bytes()
+            );
+
+            let _result = &self.pool.execute(&*query).await?;
+        }
+
+        // Update any rows of the table with matching nullifiers to have height_spent
+        for nullifier in scan_result.spent_nullifiers {
+            let height_spent = format!("{:?}", &scan_result.height);
+            let nullifier = format!("{:?}", &nullifier.to_bytes());
+
+            let result = query!(
+                r#"UPDATE notes 
+                SET height_spent = ?
+                WHERE nullifier = ?
+                RETURNING note_commitment"#,
+                height_spent,
+                nullifier
+            )
+            .fetch_all(&self.pool)
+            .await?;
+
+            //Forget spent note commitments from the NCT
+
+            nct.remove_witness(&Commitment::try_from(result[0].note_commitment.as_slice())?);
+        }
+
+        // Update NCT table with current NCT state
+
+        let query = format!(
+            "UPDATE note_commitment_tree
+            SET bytes = {:?}",
+            bincode::serialize(nct)
+        );
+
+        let _result = &self.pool.execute(&*query).await?;
+
+        Ok(self.to_owned())
     }
 }
